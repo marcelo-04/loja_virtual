329fb777e6c9a14f1f380e45f3ae0fd7f4e17566
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-04-13 22:04:43 -0300

    A primeira conexão!

---
+++ src/TestaConexao.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimeZone=UTC", 
+						"root", "P@ssw0rd25");
+				
+				System.out.println("Fechando conexão!");
+				connection.close();	
+	}
+}
