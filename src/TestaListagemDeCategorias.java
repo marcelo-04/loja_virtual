204ee8fe1c9d74332dc0fdf6c87846bdab953c6d
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-04-20 22:23:30 -0300

    Usando Inner Join.

--- src/TestaListagemDeCategorias.java
+++ src/TestaListagemDeCategorias.java
@@ -3,26 +3,20 @@ import java.sql.SQLException;
 import java.util.List;
 
 import dao.CategoriaDAO;
-import dao.ProdutoDAO;
 import modelo.Categoria;
 import modelo.Produto;
 
 public class TestaListagemDeCategorias {
 
 	public static void main(String[] args) throws SQLException {
-		
-		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
+
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
-			List<Categoria> listaDeCategorias = categoriaDAO.listar();
-			listaDeCategorias.stream().forEach(ct -> { 
+			List<Categoria> listaDeCategorias = categoriaDAO.listarComProdutos();
+			listaDeCategorias.stream().forEach(ct -> {
 				System.out.println(ct.getNome());
-				
-				try {
-					for (Produto produto : new ProdutoDAO(connection).buscar(ct)) {
-						System.out.println(ct.getNome() +" - "+ produto.getNome());
-					} 
-				} catch (Exception e) {
-					e.printStackTrace();
+				for (Produto produto : ct.getProdutos()) {
+					System.out.println(ct.getNome() + " - " + produto.getNome());
 				}
 			});
 		}
--- src/dao/CategoriaDAO.java
+++ src/dao/CategoriaDAO.java
@@ -8,32 +8,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 import modelo.Categoria;
+import modelo.Produto;
 
 public class CategoriaDAO {
-	
+
 	private Connection connection;
-	
+
 	public CategoriaDAO(Connection connection) {
-		
-		this.connection = connection;		
+
+		this.connection = connection;
 	}
-	
+
 	public List<Categoria> listar() throws SQLException {
-		
+
 		List<Categoria> categorias = new ArrayList<>();
-		
+
 		System.out.println("Executnado a query de listar categoria!");
-		
+
 		String sql = "SELECT ID, NOME FROM CATEGORIA";
-		
-		try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 			preparedStatement.execute();
-			
-			try(ResultSet resultSet = preparedStatement.getResultSet()) {
-				while(resultSet.next()) {
+
+			try (ResultSet resultSet = preparedStatement.getResultSet()) {
+				while (resultSet.next()) {
 					Categoria categoria = new Categoria(resultSet.getInt(1), resultSet.getString(2));
-					
+
 					categorias.add(categoria);
+				}
+			}
+		}
+		return categorias;
+	}
+
+	public List<Categoria> listarComProdutos() throws SQLException {
+		
+		Categoria ultimo = null;
+		List<Categoria> categorias = new ArrayList<>();
+
+		System.out.println("Executnado a query de listar categoria!");
+
+		String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO FROM CATEGORIA C INNER JOIN" 
+				+ " PRODUTO P ON C.ID = P.CATEGORIA_ID";
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+			preparedStatement.execute();
+
+			try (ResultSet resultSet = preparedStatement.getResultSet()) {
+				while (resultSet.next()) {
+					if (ultimo == null || !ultimo.getNome().equals(resultSet.getString(2))) {
+						Categoria categoria = new Categoria(resultSet.getInt(1), resultSet.getString(2));
+						ultimo = categoria;
+						categorias.add(categoria);
+					}
+					Produto produto = new Produto(resultSet.getInt(3), resultSet.getString(4), resultSet.getString(5));
+					ultimo.adicionar(produto);
 				}
 			}
 		}
